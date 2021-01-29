3eeb1d444fffed88697467c9782c29c4bf9a50b2
Vse Putyom <bertraid777@gmail.com>
2021-01-29 23:40:02 +0300

    fix( script.js): extra lines of code have been removed

--- js/script.js
+++ js/script.js
@@ -10,37 +10,31 @@
 
 //! Version 0.1 =====
 
-// let num = 266219;
-
-// let newNumbers = num.toString().split('');
-
-// let reducer = newNumbers.reduce((item, item2) => item * item2, 1); 
-
-// reducer **= 3;
-
-// let sliceReduce = String(reducer).slice(0, 2);
-
-// sliceReduce = Number(sliceReduce);
-
-// console.log(sliceReduce);
-// console.log(typeof sliceReduce);
-
-
-//? Version 0.2 =======
-
 let num = 266219;
+
+let newNumbers = num.toString().split('');
+
+let reducer = newNumbers.reduce((item, item2) => item * item2, 1); 
+
+reducer **= 3;
+
+let sliceReduce = String(reducer).slice(0, 2);
+
+console.log(sliceReduce);
+
+
+// //? Version 0.2 =======
+
+/* let num = 266219;
 const strNum = String(num);
 let result = 1;
 
 for( let i = 0; i < strNum.length; i++){
-    result = result * +strNum[i];
+    result = result * strNum[i];
 }
 
 result **= 3;
-document.write( +String(result).slice(0, 2) ); //вывод в html
-console.log(result); //вывод в консоль
-console.log(typeof result); // вывод типа данных в консоль
-
-
+console.log(( String(result).slice(0, 2)));
+*/
 
 
