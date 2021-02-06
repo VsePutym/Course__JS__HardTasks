947c0f9321c900f9017649dc525368f32ee1dc50
Vse Putyom <bertraid777@gmail.com>
2021-02-06 22:20:26 +0300

    feat(script.js): add new function

--- js/script.js
+++ js/script.js
@@ -1,50 +1,17 @@
 'use strict';
 
-// //? Task 1
-// let rus = 'ru';
-// let eng = 'en';
-// let lang = prompt('enter "ru" or "en" to select language');
-// function chooseLanguage(){
-//     if(lang === rus){
-//         return('понидельник, вторник, среда, четверг, пятница');
-//     }
-//     else if(lang === eng){
-//         return('Monday, Tuesday, Wednesday, Thursday, Friday');
-//     }
-//     else{
-//         ('incorrect data entered');
-//     }
-// }
-// console.log(chooseLanguage());
+let isNumber = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
 
-// //? Task 2
-// let rus = 'ru';
-// let eng = 'en';
-// let lang = prompt('enter "ru" or "en" to select language');
+let getString = function (arg1) {
+    if (!isNumber(arg1)) {
+        console.log(typeof arg1);
+        console.log(arg1.slice(0, 30) + '...');
+    }else{
+        console.log('it is not a string');
+    }
+    return;
+};
+getString('dwaddddddddddddwawwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwdw');
 
-// switch(lang){
-//     case 'ru':
-//         alert('понидельник, вторник, среда, четверг, пятница');
-//     break;  
-//     case 'en':
-//         alert('Monday, Tuesday, Wednesday, Thursday, Friday');
-//     break;
-// }
-
-
-// //* Task 3
-// let lang = prompt('enter "ru" or "en" to select language');
-// let rus = 'ru';
-// let eng = 'en';
-// let array = [];
-// array['ru'] = ['понидельник', 'вторник', 'среда', 'четверг', 'пятница'];
-// array['en'] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-
-// console.log(array[lang]);
-
-// //? Task 4
-
-// let namePerson = prompt('Enter your name on rus');
-// let resultName = ( namePerson === 'Артем') ? 'директор' : ( namePerson === 'Максим') ? 'преподователь': 'студент' ;
-
-// console.log(resultName);
