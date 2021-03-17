d828d8607b75fe4ba0b666fe25dbfdcf7556770f
Vse Putyom <bertraid777@gmail.com>
2021-03-18 00:26:32 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,76 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
-    const days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
-    const allMonth = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря", ];
+    const wrapper = document.querySelector('.wrapper');
+    const color = document.getElementById('color');
+    const change = document.getElementById('change');
+    const body = document.querySelector('body');
 
-    const addNewElem = () => {
-        const elem = document.createElement('div');
-        elem.classList.add('date');
-        document.body.append(elem);
-        return elem;
-    }
+    change.addEventListener('click', () =>{
+        let newColor = color.textContent;  
+        newColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+        body.style = `background-color: ${newColor}`;
+        color.innerHTML = newColor;
+    });
 
-    const dateA = addNewElem();
-    const dateB = addNewElem();
-
-    dateA.style.cssText = `
-    font-size: 40px;
+    wrapper.style.cssText = `
     text-align: center;
-    color: #902bcf;
+    margin-top: 150px;
+    `;
+
+    change.style.cssText = `
+    font-size: 30px;
+    padding: 20px;
+    outline: none;
+    border-radius: 20px`;
+
+    color.style.cssText = `
+    font-size: 50px;
     font-family: cursive;
-    font-style: italic;
-    margin-bottom: 50px`;
-
-    
-    dateB.style.cssText = `
-    font-size: 40px;
-    text-align: center;
-    color: #db0f4d;
-    font-family: cursive;
-    font-style: italic;
-    margin-bottom: 50px`;
-
-
-    const getNewAllTime = () => {
-        const date = new Date();
-        const month = allMonth[date.getMonth()];
-        let numberMonth = date.getMonth();
-        const dayWeek = days[date.getDay()];
-        let day = date.getUTCDate();
-        let hours = date.getHours();
-        let minutes = date.getMinutes();
-        let second = date.getSeconds();
-        const year = date.getUTCFullYear();
-        let min = '';
-
-        if (hours >= 5 && hours < 21) {
-            min = 'часов';
-        } else if (hours === 21 || hours === 1) {
-            min = 'час';
-        } else if (hours >= 2 && hours <= 4 || hours >= 22 && hours <= 24) {
-            min = 'часа';
-        }
-
-        if(day < 10){
-            day = `0${day}`;
-        }if(numberMonth < 10){
-            numberMonth = `0${numberMonth}`;
-        }if(hours < 10){
-            hours = `0${hours}`;
-        }if(minutes < 10){
-            minutes = `0${minutes}`;
-        }if(second < 10){
-            second = `0${second}`;
-        }
-
-        dateA.innerHTML = `сегодня ${dayWeek} ${day} ${month} ${year} года ${hours} ${min} ${minutes} минуты ${second} секунды`;
-        dateB.innerHTML = ` ${day}.${numberMonth}.${year} - ${hours}:${minutes}:${second}`;
-    }
-
-    setInterval(() => {
-        getNewAllTime();
-    }, 1000);
-
+    `;
 
 });
+
